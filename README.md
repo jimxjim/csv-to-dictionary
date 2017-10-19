246a54805df1b18c585d5891a51b84912c1d3a86
Jim <jim74255@gmail.com>
2017-10-19 11:58:48 +0800

    first commit with ezcoin file sample

---
+++ README.md
@@ -0,0 +1,15 @@
+i18n CSV to Dictionary File
+===
+
+## preparation
+
+Move your CSV file to this folder and rename it to i18n.csv.   
+Your CSV header must be locale.json   
+(locale: zh-TW, zh-CN, en-US)
+## installation
+`npm install`
+
+## run
+`npm start`
+
+Dictionary Files will be created in ./locales
---
+++ index.js
@@ -0,0 +1,43 @@
+const csvFilePath = './i18n.csv';
+const csv = require('csvtojson');
+const fs = require('fs');
+// import zhtwfile from './zh-TW.js';
+let dictJsonObjTW = '';
+let dictJsonObjCN = '';
+let dictJsonObjUS = '';
+
+const writeFile = ({path, source}) => {
+  fs.writeFile(path, source, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(`${path} was saved!`);
+  });
+}
+
+csv()
+.fromFile(csvFilePath)
+.on('json', (jsonObj) => {
+  if (jsonObj['zh-TW']['json']) {
+    dictJsonObjTW = `${dictJsonObjTW}\n${jsonObj['zh-TW']['json']}`;
+  }
+  if (jsonObj['zh-CN']['json']) {
+    dictJsonObjCN = `${dictJsonObjCN}\n${jsonObj['zh-CN']['json']}`;
+  }
+  if (jsonObj['en-US']['json']) {
+    dictJsonObjUS = `${dictJsonObjUS}\n${jsonObj['en-US']['json']}`;
+  }
+})
+.on('done', (error) => {
+  const zhtw = JSON.parse(JSON.stringify(`export default {${dictJsonObjTW}\n}`));
+  const zhcn = JSON.parse(JSON.stringify(`export default {${dictJsonObjCN}\n}`));
+  const enus = JSON.parse(JSON.stringify(`export default {${dictJsonObjUS}\n}`));
+
+  [
+    { path: './locales/zh-TW.js', source: zhtw },
+    { path: './locales/zh-CN.js', source: zhcn },
+    { path: './locales/en-US.js', source: enus },     
+  ].map(row => writeFile(row));
+
+  if (error) console.log('error');
+})
